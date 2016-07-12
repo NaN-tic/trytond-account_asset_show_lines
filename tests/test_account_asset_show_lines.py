ca558efa6672ead1f3d7f29b5b5cdf138268809d
Jes?s Mart?n Jim?nez <jesus@nan-tic.com>
2016-07-12 13:17:15 +0200

    Update test to version 4.0

--- tests/test_account_asset_show_lines.py
+++ tests/test_account_asset_show_lines.py
@@ -3,18 +3,12 @@
 # copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class TestCase(unittest.TestCase):
+class TestCase(ModuleTestCase):
     'Test module'
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('account_asset_show_lines')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+    module = 'account_asset_show_lines'
 
 
 def suite():
