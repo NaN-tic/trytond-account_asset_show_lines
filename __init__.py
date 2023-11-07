f54d709209cb5754f8eea94e773dfccc41d3f545
Raimon Esteve <raimonesteve@gmail.com>
2023-11-07 14:25:05 +0100

    Add company function field to eval rule From changeset c5c2124 #163227

--- __init__.py
+++ __init__.py
@@ -1,2 +1,10 @@
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
+from trytond.pool import Pool
+from . import account
+
+
+def register():
+    Pool.register(
+        account.AssetLine,
+        module='account_asset_show_lines', type_='model')
